daa297ba1b63414bdf0b9ddb438afdbe028ff8fb
guy.dawson <guy.dawson@theguardian.com>
2017-09-28 15:11:07 +0100

    changelog and thrift format tweaks

--- src/main/scala/com/gu/acquisition/instances/ThriftEnumFormat.scala
+++ src/main/scala/com/gu/acquisition/instances/ThriftEnumFormat.scala
@@ -3,11 +3,15 @@ package com.gu.acquisition.instances
 import com.twitter.scrooge.ThriftEnum
 import play.api.libs.json._
 
+import scala.reflect.ClassTag
+
 private[instances] trait ThriftEnumFormat {
 
-  def thriftEnumReads[A <: ThriftEnum](valueOf: String => Option[A]): Reads[A] = Reads { json =>
+  def thriftEnumReads[A <: ThriftEnum : ClassTag](valueOf: String => Option[A]): Reads[A] = Reads { json =>
     json.validate[String].flatMap { raw =>
-      valueOf(raw.filter(_ != '_')).map(JsSuccess(_)).getOrElse(JsError(""))
+      valueOf(raw.filter(_ != '_')).map(JsSuccess(_)).getOrElse {
+        JsError(s"Unable to read Thrift enum of type ${reflect.classTag[A].runtimeClass} from $raw")
+      }
     }
   }
 
